81d592f543c6782776b097910fb9cb2f09e2395f
전정헌 <69235039+allen9535@users.noreply.github.com>
2023-11-16 13:19:59 +0900

    [기능 생성] 사용자 모델 생성 (#24)

--- config/settings.py
+++ config/settings.py
@@ -16,10 +16,12 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
-CUSTOM_APPS = []
+CUSTOM_APPS = [
+    'users',
+]
 
 THIRD_PARTY_APPS = [
     'rest_framework',
@@ -113,7 +115,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
@@ -121,6 +122,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
 
 # Simple JWT 설정
 SIMPLE_JWT = {
