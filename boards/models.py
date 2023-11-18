87f8c21ceaa3cd9aded7896d97145fee32e207dc
allen9535 <allen9535@naver.com>
2023-11-18 16:18:06 +0900

    feature: 컬럼 모델 생성

--- boards/models.py
+++ boards/models.py
@@ -9,3 +9,13 @@ class Board(models.Model):
         on_delete=models.CASCADE,
         verbose_name='소유 팀'
     )
+
+
+class Column(models.Model):
+    board = models.ForeignKey(
+        Board,
+        on_delete=models.CASCADE,
+        verbose_name='보드'
+    )
+    title = models.TextField(verbose_name='제목')
+    sequence = models.PositiveIntegerField(verbose_name='순서')
