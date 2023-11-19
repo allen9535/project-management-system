e818ff63081dbba18cd5361deb71c346d8285aa4
전정헌 <69235039+allen9535@users.noreply.github.com>
2023-11-19 12:29:35 +0900

    [기능 생성] 티켓 생성 기능 생성 (#53)

--- boards/serializers.py
+++ boards/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Board, Column
+from .models import Board, Column, Ticket
 
 
 class ColumnSerializer(serializers.ModelSerializer):
@@ -9,17 +9,45 @@ class ColumnSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = '__all__'
+
+
 class BoardSerializer(serializers.ModelSerializer):
     # 보드를 그대로 직렬화하면 팀은 팀 id만 나옴
     # 따라서 별도의 메서드를 통해 어떤 값을 반환할지 결정함
     team = serializers.SerializerMethodField()
-    # 보드에 컬럼과 관련된 값은 없지만, 컬럼은 보드를 외래키로 가지고 있음
+    # 보드에 티켓과 관련된 값은 없지만, 티켓은 컬럼을 외래키로, 컬럼은 보드를 외래키로 가지고 있음
     # 이를 활용해 원하는 값을 반환함
     column = serializers.SerializerMethodField()
 
     class Meta:
         model = Board
         fields = ['team', 'column']
+
+        # 해당 데이터에서 필요한 내용만 딕셔너리 형태로 반환
+        # {
+        #    'team': '팀명',
+        #    'data': {
+        #       '컬럼명': {
+        #           'id': 컬럼 id,
+        #           'sequence': 컬럼 순서
+        #           'ticket': {
+        #               '티켓명': {
+        #                   'id': 티켓 id,
+        #                   'charge': 담장자명,
+        #                   'tag': 태그,
+        #                   'sequence': 티켓 순서,
+        #                   'volume': 작업량,
+        #                   'ended_at': 마감일,
+        #               }
+        #           }
+        #       }
+        #        ...
+        #     }
+        # }
 
     # team 필드에 어떤 값을 반환할지 결정하는 메서드
     def get_team(self, obj):
@@ -31,22 +59,42 @@ class BoardSerializer(serializers.ModelSerializer):
         # 시리얼라이저에 들어온 보드 객체에 소속된 모든 컬럼을 오름차순으로 정렬
         columns = Column.objects.filter(board=obj).order_by('sequence')
 
-        # 해당 데이터에서 필요한 내용만 딕셔너리 형태로 반환
-        # {
-        #    'team': '팀명',
-        #    'column': {
-        #       '컬럼명': {
-        #           'id': 컬럼 id,
-        #           'sequence': 컬럼 순서
-        #       }
-        #        ...
-        #     }
-        # }
         column_data = {}
+        ticket_data = {}
         for column in columns:
             column_data[column.title] = {
                 'id': column.id,
                 'sequence': column.sequence
             }
+
+            # 현재 컬럼에 해당하는 모든 티켓을 오름차순으로 정렬
+            tickets = Ticket.objects.filter(column=column).order_by('sequence')
+            for ticket in tickets:
+                # try:
+                ticket_data[ticket.title] = {}
+                # except KeyError:
+                #     print('값없음')
+                #     continue
+
+                try:
+                    ticket_data[ticket.title] = {
+                        'id': ticket.id,
+                        'tag': ticket.tag,
+                        'charge': ticket.charge.username,
+                        'volume': ticket.volume,
+                        'ended_at': ticket.ended_at,
+                        'sequence': ticket.sequence
+                    }
+                except AttributeError:
+                    ticket_data[ticket.title] = {
+                        'id': ticket.id,
+                        'tag': ticket.tag,
+                        'charge': None,
+                        'volume': ticket.volume,
+                        'ended_at': ticket.ended_at,
+                        'sequence': ticket.sequence
+                    }
+
+                column_data[column.title]['ticket'] = ticket_data
 
         return column_data
